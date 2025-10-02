aef57bb3d0f283f54e9c182b28a08df56f36e606
Phil <phildos@hotmail.co.uk>
2025-10-02 23:48:11 +0100

    Create dace-chat.tsx

---
+++ app/Components/dace-chat.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function DaveChat() {
+  return (
+    <div className="fixed bottom-4 right-4 p-4 border border-yellow-400 bg-black/70 text-white rounded-lg shadow-lg">
+      <p>DaveChat Widget (placeholder)</p>
+    </div>
+  );
+}
